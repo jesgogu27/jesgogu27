e6ff3a465c45eff427b6eba2faba5677af8ba57f
jesgogu27 <jesgogu@hotmail.com>
2022-12-19 03:15:56 -0500

    leerVector

---
+++ w3school/js/LeerVector.js
@@ -0,0 +1,11 @@
+function ImprimeVector() {
+
+    const cars = ["Saab","Volvo","BMW"];
+    for(let i = 0; i < cars.length; i++){
+        
+        return(cars[i]);
+    }
+
+}
+
+document.getElementById("demo").innerHTML = ImprimeVector();
