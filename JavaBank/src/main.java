17b23578fbbb2153c4fbb157f26000454ea0c522
jesgogu27 <jesgogu@hotmail.com>
2024-12-10 05:59:37 -0500

    salvando el Bank

--- JavaBank/src/main.java
+++ JavaBank/src/main.java
@@ -2,7 +2,6 @@
 
 public class main {
     public static void main(String[] args){
-        System.out.println("welcome to JavaBank ATM! version control with GITt");
+        System.out.println("welcome to JavaBank ATM! version control with GIT");
     }
 }
-
