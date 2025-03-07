3042111230da3765b247378d06413557ca52b2c8
jesgogu27 <jesgogu@hotmail.com>
2025-03-07 10:01:39 -0500

    clases de clases, empresas de transportes

---
+++ C#/TipoVehiculos/TipoVehiculos/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var auto = new TiposVehiculos();
+auto.cantidadRuedas = 4;
+auto.cantidaPuertas = 4;
+auto.cantidadPasajeros = 5;
+auto.capacidadCarga = 1000;
+
+
+var campero = new TiposVehiculos();
+campero.cantidaPuertas = 3;
+campero.cantidadRuedas = 4;
+campero.cantidadPasajeros = 5;
+campero.capacidadCarga = 1500;
+
+
+var empresa1 = new EmpresaTransporte();
+empresa1.Id = 1;
+empresa1.name = "Empresa1";
+empresa1.tipoServicios = "Carga";
+empresa1.clase = ClaseVehiculo.privado;
+
+List<TiposVehiculos> vehiculosEmpresa1 = new List<TiposVehiculos>();
+vehiculosEmpresa1.Add(auto);
+vehiculosEmpresa1.Add(campero);
+
+empresa1.tiposVehiculos = vehiculosEmpresa1;
+
+Console.WriteLine(empresa1.tiposVehiculos.Count);
+
+class EmpresaTransporte
+{
+    public int Id;
+    public string name;
+    public string tipoServicios;
+    public ClaseVehiculo clase;
+    public List<TiposVehiculos> tiposVehiculos;
+
+}
+
+enum ClaseVehiculo
+{
+    publico,
+    privado,
+
+}
+
+public class TiposVehiculos
+{
+    public int cantidadRuedas;
+    public int cantidadPasajeros;
+    public int cantidaPuertas;
+    public int capacidadCarga;
+
+
+}
