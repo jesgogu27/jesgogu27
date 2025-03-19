6256318f22ca69a7237a3c15623e8e779159dc1f
jesgogu27 <jesgogu@hotmail.com>
2025-03-19 03:55:48 -0500

    Reto Where

---
+++ C#/RetoWhereLinQ/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program {
+  public static void Main (string[] args) {
+
+    List<Animal> animales = new List<Animal>();
+    animales.Add(new Animal() { Nombre = "Hormiga", Color = "Rojo" });
+    animales.Add(new Animal() { Nombre = "Lobo", Color = "Gris" });
+    animales.Add(new Animal() { Nombre = "Elefante", Color = "Gris" });
+    animales.Add(new Animal() { Nombre = "Pantegra", Color = "Negro" });
+    animales.Add(new Animal() { Nombre = "Gato", Color = "Negro" });
+    animales.Add(new Animal() { Nombre = "Iguana", Color = "Verde" });
+    animales.Add(new Animal() { Nombre = "Sapo", Color = "Verde" });
+    animales.Add(new Animal() { Nombre = "Camaleon", Color = "Verde" });
+    animales.Add(new Animal() { Nombre = "Gallina", Color = "Blanco" });
+
+    // Escribe tu código aquí
+    // filtra todos los animales que sean de color verde que su nombre inicie con una vocal.
+    
+    var result = Imprimir(animales);
+
+    foreach(var x in result) {
+        Console.WriteLine(x.Nombre + ", " + x.Color);
+    }
+  }
+
+    public static List<Animal> Imprimir(List<Animal> animals){
+
+        return animals.Where(x => x.Color.Contains("Verde") && "AEIOUaeiou".Contains(x.Nombre[0])).ToList();
+    }
+  public class Animal
+  {
+    public string Nombre {get;set;}
+    public string Color {get;set;}
+  }
+}
